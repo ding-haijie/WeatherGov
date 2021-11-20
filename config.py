814224e7632f1a555ac2f8c87c7a87a2c3f86cc9
dinghj <dinghjbyte@outlook.com>
2021-11-21 00:38:28 +0800

    chore: params

--- config.py
+++ config.py
@@ -6,14 +6,13 @@ params = {
     'hidden_dim': 500,
     'dropout': 0.6,
     'weight_decay': 0.0,
-    'gamma': 8.5,
-    'clip': 1.0,
+    'gamma': 4,
+    'clip': 5.0,
     'seed': 1,
     'beam_width': 1,
     'num_info': 36,
     'max_len': 89,
     'input_dim': 218,
     'output_dim': 361,
-    'train_mode': False,
-    'resume': True
+    'resume': False
 }
